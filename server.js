e2b4df445f6ec9f7298f5b8737a4ffc348733aa2
turtles <me@ramon.nyc>
2021-02-03 21:04:23 -0800

    added routes folder with sample routes js files.

--- server.js
+++ server.js
@@ -12,56 +12,24 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Notes (DATA)
-// =============================================================
-var noateData = [
-    {
-        noteTitle : "Natalia",
-        noteText : "123-456-7890",
-        noteID : "1",
-    },
-    {
-        noteTitle : "Natalia",
-        noteText : "123-456-7890",
-        noteID : "2",
-    }
-];
+// Sets up the Express app to serve static css files
+app.use(express.static(__dirname + "/public"));
 
 // Routes
 // =============================================================
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    // Sends to welcome page
+    // Sends users to welcome page
     res.sendFile(path.join(__dirname, "index.html"));
   });
-  
-  app.get("/hi", function(req, res) {
-    res.sendFile(path.join(__dirname, "hi.html"));
+    // Sends users to notes page
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
   });
   
-  // Displays all characters
-  app.get("/api/characters", function(req, res) {
-    return res.json(characters);
-  });
-  
-  // Displays a single character, or returns false
-  app.get("/api/characters/:character", function(req, res) {
-    var chosen = req.params.character;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < characters.length; i++) {
-      if (chosen === characters[i].routeName) {
-        return res.json(characters[i]);
-      }
-    }
-  
-    return res.json(false);
-  });
-  
-  // Create New Characters - takes in JSON input
-  app.post("/api/characters", function(req, res) {
+  // Create New Note - takes in SOMETHING input
+  app.post("/api/notes", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newcharacter = req.body;
