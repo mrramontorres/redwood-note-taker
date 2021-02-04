979b9d4f3decb58cf46a32a5697fa12a7d85e740
turtles <me@ramon.nyc>
2021-02-03 19:06:10 -0800

    copied over starter code from class activity star wars.

---
+++ db/db.js
@@ -0,0 +1,14 @@
+var noateData = [
+    {
+        noteTitle : "Natalia",
+        noteText : "123-456-7890",
+        noteID : "1",
+    },
+    {
+        noteTitle : "Natalia",
+        noteText : "123-456-7890",
+        noteID : "2",
+    }
+];
+
+module.exports = noteData
